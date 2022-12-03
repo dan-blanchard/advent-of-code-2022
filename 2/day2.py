e90bc63b054a36298363190e47a5f6c14231f5aa
Dan Blanchard <dan.blanchard@gmail.com>
2022-12-02 23:06:15 -0500

    DRY

--- 2/day2.py
+++ 2/day2.py
@@ -5,31 +5,20 @@ A_ORD = ord("A")
 X_ORD = ord("X")
 
 
-def score_round1(line):
+def score_round(line, second_is_outcome):
     opponent, own = line.split()
     opponent_shape = ord(opponent) - A_ORD
-    own_shape = ord(own) - X_ORD
-    # Win
-    if own_shape == (opponent_shape + 1) % 3:
-        score = 6
-    # Draw
-    elif own_shape == opponent_shape:
-        score = 3
-    # Lose
+    # If the second field on the line is actually the outcome,
+    # and not what we should play, figure out what we should play
+    if second_is_outcome:
+        if own == "X":
+            own_shape = (opponent_shape - 1) % 3
+        elif own == "Y":
+            own_shape = opponent_shape
+        else:
+            own_shape = (opponent_shape + 1) % 3
     else:
-        score = 0
-    return score + (own_shape + 1)
-
-
-def score_round2(line):
-    opponent, lose_draw_win = line.split()
-    opponent_shape = ord(opponent) - A_ORD
-    if lose_draw_win == "X":
-        own_shape = (opponent_shape - 1) % 3
-    elif lose_draw_win == "Y":
-        own_shape = opponent_shape
-    else:
-        own_shape = (opponent_shape + 1) % 3
+        own_shape = ord(own) - X_ORD
     # Win
     if own_shape == (opponent_shape + 1) % 3:
         score = 6
@@ -54,9 +43,9 @@ def main():
     )
     args = parser.parse_args()
     lines = [line.strip() for line in fileinput.input(args.files)]
-    round_scores = [score_round1(line) for line in lines]
+    round_scores = [score_round(line, False) for line in lines]
     print(f"Part A score sum: {sum(round_scores)}")
-    round_scores = [score_round2(line) for line in lines]
+    round_scores = [score_round(line, True) for line in lines]
     print(f"Part B score sum: {sum(round_scores)}")
 
 
